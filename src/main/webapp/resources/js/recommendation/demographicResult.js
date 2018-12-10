3af88075ebf4acf0f2d64776aac5d9ad8ee56706
SoYeonV <sodus0131@naver.com>
2018-12-10 09:01:52 +0900

    세은이 브랜치 합치기전

--- src/main/webapp/resources/js/recommendation/demographicResult.js
+++ src/main/webapp/resources/js/recommendation/demographicResult.js
@@ -117,50 +117,6 @@ function analysisResultText(){
 $('#recommendButton').on('click', function(event) {
 	event.preventDefault();
 
-	/*
-	var String = '<div class="col-sm-4 output step-txt"style="min-height: 589px;">'
-			   + '<form role="form" class="ng-pristine ng-valid">'
-			   + '<div class="analysResultDiv">'
-			   + '<p class="txt-board">'
-			   + '<span id="isMarriedComment"></span><br>'
-			   + '<span id="userNameResult" class="info-txt typeName ng-binding"></span>'
-			   + '님에게  꼭 필요한 보험은<br>' 
-			   + '<span id="insuranceTypeFirst" class="info-txt ng-binding"></span>,' 
-			   + '<span id="insuranceTypeSecond" class="info-txt ng-binding"></span> 보험이며,<br>'
-			   + '고려해볼만한 보험은<br>' 
-			   + '<span id="insuranceTypeThird" class="info-txt ng-binding"></span>,'
-			   + '<span id="insuranceTypeFourth" class="info-txt ng-binding"></span><br> 보험입니다.'
-			   + '</p>'
-			   + '</div>'
-			   + '</form>'
-			   + '</div>'
-			   + '<div class="col-sm-8 forms step-area" style="min-height: 589px;">'
-			   + '<div class="bgChange">'
-			   + '<div class="title-wrapper">'
-	           + '<h4 class="recommendation-header">'
-	           + '<span class="application-name">I\'M SURE</span>에서 확신하는' 
-	           + '<span class="recommendation-highlight">추천</span> 생명보험은?'
-	           + '</h4>'
-	           + '<button type="button" class="btn" data-toggle="modal"'
-	           + 'data-target="#algorithmDescriptionModal">Why SURE ?</button>'
-	           + '</div>'
-			   + '<ul class="nav nav-tabs">'
-			   + '<li class="step-tab-v2">암/성인병</li>'
-			   + '<li class="step-tab-v2">질병</li>'
-			   + '<li class="step-tab-v2">연금</li>'
-			   + '<li class="step-tab-v2">저축</li>'
-			   + '<li class="step-tab-v2">더보기</li>'
-			   + '</ul>'
-			   + '<div class="form-area step1 on">'
-			   + '<div class="recommendation-list"></div>'
-			   + '</div>'
-			   + '<button class="buttonPrev"><span> 이전</span></button>'
-			   + '</div>'
-			   + '</div>';
-	$('.row').empty();
-	$('.row').append(String);
-	analysisResultText();
-	*/
 });
 
 
